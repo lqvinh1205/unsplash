8ce7ea56e9cf443e617d11cbe63e1d407c31399f
vinhlq <quangvinh778891@gmail.com>
2023-09-21 22:23:21 +0700

    config tailwind

---
+++ postcss.config.ts
@@ -0,0 +1,6 @@
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+const postcssConfig = {
+  plugins: [tailwindcss, autoprefixer],
+};
+export default postcssConfig;
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+// Add PostCSS configuration
+import path from "path";
+import postcssConfig from "./postcss.config";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: postcssConfig,
+  },
   resolve: {
     alias: {
-      "@": "/src", // This is where you specify the "@" symbol as an alias for the root directory.
+      "@": path.resolve(__dirname, "/src"), // This is where you specify the "@" symbol as an alias for the root directory.
     },
   },
 });
